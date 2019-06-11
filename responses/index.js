0ed0dd15cdce58e78ea0bb9c49d37f01b2bb349c
cyberlight <cyberlight@live.ru>
2019-06-11 07:03:09 +0000

    #4 Fixed requiring json files using sync file read instead of require
    * Fixed syntax

--- responses/index.js
+++ responses/index.js
@@ -1,10 +1,11 @@
-const path = require('path');
-const modules = {}
-require('fs').readdirSync(__dirname).forEach(function (file) {
-    if (file.match(/\.json$/) !== null && file !== 'index.js') {
-        var name = file.replace('.json', '');
-        modules[name] = require(path.join(__dirname, file));
-    }
+const fs = require("fs");
+const path = require("path");
+const modules = {};
+require("fs").readdirSync(__dirname).forEach(function (file) {
+  if (file.match(/\.json$/) !== null && file !== "index.js") {
+    var name = file.replace(".json", "");
+    modules[name] = JSON.parse(fs.readFileSync(path.join(__dirname, file)));
+  }
 });
 
 module.exports = modules;

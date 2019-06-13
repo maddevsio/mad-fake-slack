c67d3dd7f25e464a6ffaa7bc1ba1dbd60f1e11e9
cyberlight <cyberlight@live.ru>
2019-06-13 06:19:15 +0000

    #12 Added new powerfull helpers for "if" instruction
    * For example:
    ```
    {{#if (or section1 section2)}}
    .. content
    {{/if}}
    ```
    or
    ```
    {{#if (or
            (eq section1 "foo")
            (ne section2 "bar"))}}
    .. content
    {{/if}}
    ```

--- helpers.js
+++ helpers.js
@@ -1,22 +1,38 @@
+const moment = require("moment");
+
 module.exports = {
   "json": function (context) {
     return JSON.stringify(context);
   },
-  "if": function (conditional, options) {
-    if (conditional) {
-      return options.fn(this);
-    } else {
-      return options.inverse(this);
-    }
-  },
-  "ifeq": function (left, right, options) {
-    if (left === right) {
-      return options.fn(this);
-    } else {
-      return options.inverse(this);
-    }
-  },
   "concat": function (...args) {
     return args.slice(0, -1).join("");
+  },
+  eq: function (v1, v2) {
+    return v1 === v2;
+  },
+  ne: function (v1, v2) {
+    return v1 !== v2;
+  },
+  lt: function (v1, v2) {
+    return v1 < v2;
+  },
+  gt: function (v1, v2) {
+    return v1 > v2;
+  },
+  lte: function (v1, v2) {
+    return v1 <= v2;
+  },
+  gte: function (v1, v2) {
+    return v1 >= v2;
+  },
+  and: function () {
+    return Array.prototype.slice.call(arguments).every(Boolean);
+  },
+  or: function () {
+    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+  },
+  toHumanTime: function (timestamp, options) {
+    const unixts = +timestamp.split(".")[0];
+    return moment.unix(unixts).format("h:mm A");
   }
 };

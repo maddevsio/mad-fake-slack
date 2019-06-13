dd469ef63cb3becbb2970c43ea3fc65431518c17
cyberlight <cyberlight@live.ru>
2019-06-13 06:27:32 +0000

    #12 Added usage messages inside db manager

--- db/index.js
+++ db/index.js
@@ -8,4 +8,25 @@ require("fs").readdirSync(__dirname).forEach(function (file) {
   }
 });
 
+const messages = require("./messages");
+modules["messages"] = messages;
+modules["manager"] = {
+  channel (channelId) {
+    return {
+      messages (total) {
+        return Object
+          .values(modules.messages[channelId])
+          .slice(1, Number.isInteger(total) ? total + 1 : total).map(m => {
+            const user = modules.users.filter(u => u.id === m.user_id)[0];
+            const team = modules.teams.filter(t => t.id === user.team_id)[0];
+            return {
+              ...m,
+              user,
+              team
+            };
+          });
+      }
+    };
+  }
+};
 module.exports = modules;

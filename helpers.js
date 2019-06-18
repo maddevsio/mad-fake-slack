f48d9207f75bec5997571bd6668fe2dfb3b3d9bd
cyberlight <cyberlight@live.ru>
2019-06-18 07:24:36 +0000

    #14 Added usage chared helpers from public folder inside server helpers

--- helpers.js
+++ helpers.js
@@ -1,43 +1,15 @@
-const moment = require("moment");
-let id = 0;
+const path = require("path");
+const fs = require("fs");
+const clientHelpers = require("./public/js/helpers.js");
 
 module.exports = {
-  "json": function (context) {
-    return JSON.stringify(context);
-  },
-  "concat": function (...args) {
-    return args.slice(0, -1).join("");
-  },
-  eq: function (v1, v2) {
-    return v1 === v2;
-  },
-  ne: function (v1, v2) {
-    return v1 !== v2;
-  },
-  lt: function (v1, v2) {
-    return v1 < v2;
-  },
-  gt: function (v1, v2) {
-    return v1 > v2;
-  },
-  lte: function (v1, v2) {
-    return v1 <= v2;
-  },
-  gte: function (v1, v2) {
-    return v1 >= v2;
-  },
-  and: function () {
-    return Array.prototype.slice.call(arguments).every(Boolean);
-  },
-  or: function () {
-    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
-  },
-  makeTs: function () {
-    id += 1;
-    return `${Math.round(+new Date() / 1000)}.${String(id).padStart(6, "0")}`;
-  },
-  toHumanTime: function (timestamp, options) {
-    const unixts = +timestamp.split(".")[0];
-    return moment.unix(unixts).format("h:mm A");
+  ...clientHelpers,
+  include: function (...args) {
+    const parts = args.slice(0, -1);
+    let filePath = path.join("views", ...parts);
+    if (!filePath.endsWith(".hbs")) {
+      filePath = `${filePath}.hbs`;
+    }
+    return fs.readFileSync(filePath, { encoding: "utf-8" });
   }
 };

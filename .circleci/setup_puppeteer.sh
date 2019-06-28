8986c341d203d5ab696b6435e19bfd9cf9d7dc8f
Alexander <cyberlight@live.ru>
2019-06-28 13:06:07 +0600

    #24 Remove installing wget

--- .circleci/setup_puppeteer.sh
+++ .circleci/setup_puppeteer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install wget
 sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
     && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
     && apt-get update \

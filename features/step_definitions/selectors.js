1bd5ce197879bcb064b8a6d93ca595dd16afed54
cyberlight <cyberlight@live.ru>
2019-06-28 06:25:08 +0000

    #22 Selectors moved to css selectors intead of xpath for simplicity

--- features/step_definitions/selectors.js
+++ features/step_definitions/selectors.js
@@ -1,16 +1,27 @@
 const mainSelectors = {
-  "Team name place": "and contains(@id, 'team_name')",
-  channelSections: (names) => {
-    let parts = names.map(name => `(normalize-space(string(.)) = '${name}')`);
-    if (parts.length > 1) {
-      parts = parts.join(" or ");
-      return `//*[contains(@class, 'p-channel_sidebar__channel') and (${parts})]`;
-    } else {
-      parts = parts[0];
-      return `//*[contains(@class, 'p-channel_sidebar__channel') and ${parts}]`;
-    }
-  },
-  "channel_section": "//*[contains(@class, 'p-channel_sidebar__channel')]"
+  "Team name": "#team_name",
+  "Team user name": "#team_menu_user_name",
+  "Selected channel": ".p-channel_sidebar__channel--selected",
+  "Channel title": "#channel_title",
+  "Favorite": ".im.im-star-o",
+  "Channel header information": "#channel_header_info",
+  "Users": ".im.im-user-male",
+  "Channel members count": "#channel_members_toggle_count",
+  "Channel header topic": "#channel_topic_text",
+  "Channel header buttons": ".channel_header__buttons",
+  "Recent mentions button": "button#recent_mentions_toggle > .im.im-link",
+  "Stars toggle button": "button#stars_toggle > .im.im-star-o",
+  "Flex toggle button": "button#flex_menu_toggle > .im.im-menu-dot-v",
+  "Messages container": "#messages_container",
+  "Message item": ".list__item",
+  "Input message container": "#msg_form",
+  "Input message": "#msg_input_text",
+  "File upload button": "#primary_file_button",
+  "Emoji button": "#main_emo_menu",
+  "User mentions button": ".msg_mentions_button",
+  "Notification bar container": "#notification_bar",
+  "Left notification section": ".p-notification_bar__section.p-notification_bar__section--left",
+  "Right notification section": ".p-notification_bar__section.p-notification_bar__section--right"
 };
 
 const selectors = {

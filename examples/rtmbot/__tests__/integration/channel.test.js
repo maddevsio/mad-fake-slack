66ecc52e4380e10afdd776b57e4180dcd0f25d9b
cyberlight <cyberlight@live.ru>
2019-07-03 05:15:20 +0000

    #34 Reformat code of example bot

--- examples/rtmbot/__tests__/integration/channel.test.js
+++ examples/rtmbot/__tests__/integration/channel.test.js
@@ -1,37 +1,40 @@
-const { startBot, waitMs, resetDb } = require("./utils");
+const { startBot, waitMs, resetDb } = require('./utils');
 
-describe("Channel communication", () => {
+describe('Channel communication', () => {
   let bot = null;
 
   beforeEach(async () => {
     await resetDb();
-    await page.goto("http://0.0.0.0:9001");
+    await page.goto('http://0.0.0.0:9001');
     bot = await startBot();
   });
 
-  describe("general channel", () => {
-    describe("when user sent message", () => {
-      it("bot should display echo message", async () => {
-        await page.keyboard.type("Hello");
-        await page.keyboard.press("Enter");
-        await expect(page).toMatchElement("span.c-message__body", { text: "You sent text to the channel: Hello" });
+  describe('general channel', () => {
+    describe('when user sent message', () => {
+      it('bot should display echo message', async () => {
+        await page.keyboard.type('Hello');
+        await page.keyboard.press('Enter');
+        await expect(page).toMatchElement('span.c-message__body',
+          { text: 'You sent text to the channel: Hello' });
       });
     });
   });
 
-  describe("random channel", () => {
-    describe("when user sent message", () => {
-      it("bot should display echo message", async () => {
+  describe('random channel', () => {
+    describe('when user sent message', () => {
+      it('bot should display echo message', async () => {
         await await Promise.all([
-          page.waitForNavigation({ waitUntil: "load" }),
-          expect(page).toClick("span.p-channel_sidebar__name", { text: "random" })
+          page.waitForNavigation({ waitUntil: 'load' }),
+          expect(page).toClick('span.p-channel_sidebar__name', { text: 'random' })
         ]);
-        await page.keyboard.type("Hello from random!");
-        await page.keyboard.press("Enter");
-        await expect(page).toMatchElement("span.c-message__body", { text: "You sent text to the channel: Hello from random!" });
-        const messages = await page.$$eval("span.c-message__body", spans => Array.from(spans).map(el => el.textContent));
-        expect(messages.length).toBe(6);
-        expect(messages[messages.length - 1].trim()).toEqual("You sent text to the channel: Hello from random!");
+        await page.keyboard.type('Hello from random!');
+        await page.keyboard.press('Enter');
+        await expect(page).toMatchElement('span.c-message__body',
+          { text: 'You sent text to the channel: Hello from random!' });
+        const messages = await page.$$eval('span.c-message__body',
+          spans => Array.from(spans).map(el => el.textContent));
+        expect(messages).toHaveLength(6);
+        expect(messages[messages.length - 1].trim()).toEqual('You sent text to the channel: Hello from random!');
       });
     });
   });
@@ -41,6 +44,6 @@ describe("Channel communication", () => {
       await bot.destroy();
       await waitMs(1000);
       await resetDb();
-    };
+    }
   });
 });
--- examples/rtmbot/index.js
+++ examples/rtmbot/index.js
@@ -1,8 +1,8 @@
-const { WebClient, LogLevel } = require("@slack/web-api");
-const { RTMClient } = require("@slack/rtm-api");
+const { WebClient, LogLevel } = require('@slack/web-api');
+const { RTMClient } = require('@slack/rtm-api');
 
-const BOT_TOKEN = "xoxb-XXXXXXXXXXXX-TTTTTTTTTTTTTT";
-const SLACK_API = process.env.SLACK_API || "http://localhost:9001/api/";
+const BOT_TOKEN = 'xoxb-XXXXXXXXXXXX-TTTTTTTTTTTTTT';
+const SLACK_API = process.env.SLACK_API || 'http://localhost:9001/api/';
 
 const web = new WebClient(BOT_TOKEN, { slackApiUrl: SLACK_API, logLevel: LogLevel.DEBUG });
 const rtm = new RTMClient(BOT_TOKEN, { slackApiUrl: SLACK_API, logLevel: LogLevel.DEBUG });
@@ -11,26 +11,36 @@ const rtm = new RTMClient(BOT_TOKEN, { slackApiUrl: SLACK_API, logLevel: LogLeve
 // When the connection is active, the 'ready' event will be triggered
 let botUserId = null;
 
-rtm.on("ready", async () => {
-  const { user_id: userId, user: name } = await web.auth.test();
+rtm.on('ready', async () => {
+  const { user_id: userId, user: userName } = await web.auth.test();
   botUserId = userId;
-  console.warn("[i] bot userId:", userId, " and user name ", name);
+  // eslint-disable-next-line no-console
+  console.warn('[i] bot userId:', userId, ' and user name ', userName);
   const listOfChannels = await web.channels.list({ exclude_archived: 1 });
-  const { id } = listOfChannels.channels.filter(({ id, name }) => name === "general")[0];
-  console.warn("Channnel ID: ", id);
-  const res = await rtm.sendMessage("Hello there! I am a Valera!", id);
-  console.warn("[i] Message sent: ", res.ts);
+  const { id } = listOfChannels.channels.filter(({ name }) => name === 'general')[0];
+  // eslint-disable-next-line no-console
+  console.warn('Channnel ID: ', id);
+  const res = await rtm.sendMessage('Hello there! I am a Valera!', id);
+  // eslint-disable-next-line no-console
+  console.warn('[i] Message sent: ', res.ts);
 });
 
-rtm.on("message", async (event) => {
-  const res = await rtm.sendMessage(`You sent text to ${botUserId === event.channel ? "me (direct)" : "the channel"}: ${event.text}`, event.channel);
-  console.warn("[i] Message sent: ", res.ts);
+rtm.on('message', async (event) => {
+  const res = await rtm.sendMessage(
+    `You sent text to ${botUserId === event.channel
+      ? 'me (direct)'
+      : 'the channel'}: ${event.text}`, event.channel
+  );
+  // eslint-disable-next-line no-console
+  console.warn('[i] Message sent: ', res.ts);
 });
 
 // After the connection is open, your app will start receiving other events.
-rtm.on("user_typing", (event) => {
+rtm.on('user_typing', (event) => {
+  // eslint-disable-next-line no-console
   console.log(event);
 });
 
 rtm.start()
+  // eslint-disable-next-line no-console
   .catch(console.error);

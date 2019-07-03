456930be6bd06b81999873287f47d7cb0b8f0d90
cyberlight <cyberlight@live.ru>
2019-07-03 05:11:08 +0000

    #34 Changed eslint base rules to airbnb-base/legacy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,38 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "jest": true
-    },
-    "extends": "standard",
-    "plugins": [
-      "html"
-    ],
-    "settings": {
-        "html/html-extensions": [".hbs"],
-    },
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "page": true,
-        "browser": true,
-        "context": true,
-        "jestPuppeteer": true,
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-    }
+  "extends": ["airbnb-base/legacy"],
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "jest": true
+  },
+  "plugins": ["jest", "html" ],
+  "settings": {
+    "html/html-extensions": [".hbs"],
+  },
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+    "page": true,
+    "browser": true,
+    "context": true,
+    "jestPuppeteer": true,
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+    "no-restricted-syntax": "warn",
+    "max-len": [
+      2,
+      180,
+      8
+    ]
+  }
 };

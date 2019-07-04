ea0f36868d93cfcb090dc06c661d1babcb1e7e15
cyberlight <cyberlight@live.ru>
2019-07-04 03:03:31 +0000

    #34 I decided to temporarily add templates with partial inclusion to the ignore list for linting
    * Because more time needed for analog of partial template function

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: 'recommended',
-
+  ignore: [
+    'views/layouts/main',
+    'views/slackv2'
+  ],
   rules: {
     'no-bare-strings': false,
     'no-triple-curlies': false,

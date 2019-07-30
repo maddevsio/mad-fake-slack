5f625655232b4d8ffaa259e6ae13138246035300
cyberlight <cyberlight@live.ru>
2019-07-30 07:32:59 +0000

    #58 Added lexer and changed formatter
    * Removed usage of regexps

--- public/js/formatters.js
+++ public/js/formatters.js
@@ -1,36 +1,225 @@
+const STRIKE = '~';
+const ITALIC = '_';
+const BOLD = '*';
+const CODE = '`';
+const PREFORMATTED = '```';
+const QUOTE = '>';
+const TEXT_TYPE = 'TEXT';
+
+const tokens = {
+  STRIKE: STRIKE,
+  ITALIC: ITALIC,
+  BOLD: BOLD,
+  CODE: CODE,
+  PREFORMATTED: PREFORMATTED,
+  QUOTE: QUOTE
+};
+
+const delimiters = [
+  {
+    type: 'PREFORMATTED',
+    startToken: PREFORMATTED,
+    endToken: PREFORMATTED,
+    checkStarts() { return true; }
+  },
+  {
+    type: 'CODE',
+    startToken: CODE,
+    endToken: CODE,
+    checkStarts() { return true; }
+  },
+  {
+    type: 'STRIKE',
+    startToken: STRIKE,
+    endToken: STRIKE,
+    checkStarts() { return true; }
+  },
+  {
+    type: 'ITALIC',
+    startToken: ITALIC,
+    endToken: ITALIC,
+    checkStarts() { return true; }
+  },
+  {
+    type: 'BOLD',
+    startToken: BOLD,
+    endToken: BOLD,
+    checkStarts() { return true; }
+  },
+  {
+    type: 'QUOTE',
+    startToken: QUOTE,
+    endToken: '\n',
+    continious: true,
+    checkStarts(index, text) {
+      let end = index;
+      while (end >= 0) {
+        end -= 1;
+        const char = text.charAt(end);
+        if (char === '\n' || char !== ' ') {
+          break;
+        }
+      }
+      return text.substring(end, index).trim() === '';
+    }
+  }
+];
+
+function byDelimeter(text, delim, i) {
+  return text.substr(i, delim.startToken.length) === delim.startToken && delim.checkStarts(i, text);
+}
+
+class Lexer {
+  constructor() {
+    this.partText = '';
+    this.partStart = null;
+    this.partEnd = null;
+    this.lexems = [];
+  }
+
+  static mapToken(text, index, token) {
+    const delimeterLength = token.endToken.length;
+    let currentIndex = index;
+    while (currentIndex < text.length) {
+      if (text.substr(currentIndex, delimeterLength) === token.endToken) {
+        break;
+      }
+      currentIndex += 1;
+    }
+    return currentIndex - index;
+  }
+
+  static getContent(lexem) {
+    switch (lexem.type) {
+      case 'QUOTE': {
+        return lexem.text.split('\n').map(line => line.trim().slice(1)).join('\n');
+      }
+      case 'TEXT': {
+        return lexem.text;
+      }
+      default: {
+        const token = tokens[lexem.type];
+        const tokenLength = token.length;
+        return lexem.text.trim().slice(tokenLength, lexem.text.length - tokenLength);
+      }
+    }
+  }
+
+  static getPositions(start, text, token) {
+    let end = start + token.startToken.length;
+    const mapLength = Lexer.mapToken(text, end, token);
+    end += mapLength + token.endToken.length;
+    return [start, end];
+  }
+
+  clearVariables() {
+    this.partText = '';
+    this.partType = null;
+    this.partStart = null;
+    this.partEnd = null;
+  }
+
+  pushLexem(type, start, end, text, skipEmpty = false) {
+    if (skipEmpty && !text) return;
+    const lexem = {
+      type,
+      start,
+      end,
+      text
+    };
+    lexem.content = Lexer.getContent(lexem);
+    this.lexems.push(lexem);
+  }
+
+  accumulatePartialText(type, text, start, end) {
+    this.partType = type;
+    this.partStart = this.partStart || start;
+    this.partEnd = end;
+    this.partText += text.substring(start, end);
+  }
+
+  trackText(otherText) {
+    if (otherText) {
+      this.pushLexem(TEXT_TYPE, 0, 0, otherText);
+    }
+  }
+
+  trackBlock({
+    block, start, end, text
+  }) {
+    if (block.continious) {
+      this.accumulatePartialText(block.type, text, start, end);
+    } else {
+      this.pushLexem(this.partType, this.partStart, this.partEnd, this.partText, true);
+      this.pushLexem(block.type, start, end, text.substring(start, end));
+      this.clearVariables();
+    }
+  }
+
+  trackPartText() {
+    this.pushLexem(this.partType, this.partStart, this.partEnd, this.partText, true);
+  }
+
+  lex(text) {
+    let otherText = '';
+    for (let i = 0; i < text.length;) {
+      const [block] = delimiters.filter(delim => byDelimeter(text, delim, i));
+      if (block) {
+        const [start, end] = Lexer.getPositions(i, text, block);
+        this.trackText(otherText);
+        otherText = '';
+        this.trackBlock({
+          block, start, end, text
+        });
+        i = end;
+      } else {
+        this.trackPartText();
+        this.clearVariables();
+        otherText += text.charAt(i);
+        i += 1;
+      }
+    }
+    this.trackText(otherText);
+    this.trackPartText();
+    this.clearVariables();
+
+    const result = this.lexems.slice();
+    this.lexems = [];
+    return result;
+  }
+}
+
 class MdFormatter {
   constructor() {
-    this.rules = Object.entries(Object.getOwnPropertyDescriptors(MdFormatter))
-      .reduce((props, [key]) => props.concat(key.startsWith('rule') ? [key] : []), []);
-  }
-
-  static rulePre(text) {
-    // eslint-disable-next-line no-useless-escape
-    return text.replace(/[`]{3}([^`]+)[`]{3}(?!.*\<\/code\>)/g, '<pre>$1</pre>');
-  }
-
-  static ruleCode(text) {
-    // eslint-disable-next-line no-useless-escape
-    return text.replace(/[`]{1}([^`]+)[`]{1}/g, '<code>$1</code>');
-  }
-
-  static ruleBold(text) {
-    // eslint-disable-next-line no-useless-escape
-    return text.replace(/[\*]{1}([^\*]+)[\*]{1}(?!.*\<\/(pre|code)\>)/g, '<b>$1</b>');
-  }
-
-  static ruleItalic(text) {
-    // eslint-disable-next-line no-useless-escape
-    return text.replace(/[_]{1}([^_]+)[_]{1}(?!.*\<\/(pre|code)\>)/g, '<i>$1</i>');
-  }
-
-  static ruleStrike(text) {
-    // eslint-disable-next-line no-useless-escape
-    return text.replace(/[~]{1}([^~]+)[~]{1}(?!.*\<\/(pre|code)\>)/g, '<s>$1</s>');
+    this.lexer = new Lexer();
+    this.formatters = {
+      PREFORMATTED(content) {
+        return `<pre class="c-mrkdwn__pre">${content}</pre>`;
+      },
+      CODE(content) {
+        return `<code class="c-mrkdwn__code">${content}</code>`;
+      },
+      QUOTE(content) {
+        return `<blockquote class="c-mrkdwn__quote">${content}</blockquote>`;
+      },
+      STRIKE(content) {
+        return `<s>${content}</s>`;
+      },
+      BOLD(content) {
+        return `<b>${content}</b>`;
+      },
+      ITALIC(content) {
+        return `<i>${content}</i>`;
+      },
+      TEXT(content) {
+        return content;
+      }
+    };
   }
 
   format(text) {
-    return this.rules.reduce((formatted, key) => MdFormatter[key](formatted), text);
+    const lexems = this.lexer.lex(text);
+    return lexems.reduce((formatted, block) => `${formatted}${this.formatters[block.type](block.content)}`, '');
   }
 }
 

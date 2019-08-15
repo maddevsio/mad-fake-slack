6bd1986033656f34098b8d7acc3fa2fa67461da4
cyberlight <cyberlight@live.ru>
2019-08-15 06:26:24 +0000

    #75 Added action to set cursor position by index

--- features/step_definitions/actions.js
+++ features/step_definitions/actions.js
@@ -515,6 +515,19 @@ function runTimes(times, action) {
   return Promise.mapSeries(calls, c => c());
 }
 
+function setTextPositionTo(position) {
+  const page = scope.context.currentPage;
+  return page.evaluate(pos => {
+    const inputField = window.msg_input_text;
+    const textLength = inputField.value.length || 1;
+    const mapTextPosition = textPos => ({ starting: 1, ending: textLength }[textPos] || 1);
+    // eslint-disable-next-line no-restricted-globals
+    const textPosition = !isNaN(pos) ? Number(pos) : mapTextPosition(pos);
+    inputField.focus();
+    inputField.selectionEnd = textPosition;
+  }, position);
+}
+
 module.exports = {
   wait,
   goToUrl,
@@ -553,5 +566,6 @@ module.exports = {
   getMemorizeProperty,
   getPropertyValueBySelector,
   setFocus,
-  runTimes
+  runTimes,
+  setTextPositionTo
 };

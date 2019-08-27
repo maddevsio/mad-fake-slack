6660ea775162ecb80db3ff7d733c3ed8c056dacb
cyberlight <cyberlight@live.ru>
2019-08-27 04:09:50 +0000

    #83 Added GET /api/db/current/team api for changing team domain

--- routes/testapi/index.js
+++ routes/testapi/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 const { dbManager } = require('../managers');
 
-router.get('/api/db/reset', (req, res) => {
+router.get('/reset', (req, res) => {
   dbManager.reset();
   res.json({ ok: true });
+});
+
+router.get('/current/team', (req, res) => {
+  if (typeof req.query.domain !== 'undefined') {
+    dbManager.slackTeam().domain = req.query.domain;
+    res.json({
+      ok: true
+    });
+  } else {
+    res.json({
+      ok: false
+    });
+  }
 });
 
 module.exports = router;

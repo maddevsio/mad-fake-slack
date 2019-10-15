045bd21116906379e4fc69d976889cecfebce97a
CyberLight <cyberlight@live.ru>
2019-10-15 06:52:44 +0000

    #107 Added custom format for slacksubtypes + "schemas for $refs"

--- features/step_definitions/support/validators.js
+++ features/step_definitions/support/validators.js
@@ -25,7 +25,60 @@ module.exports = {
     Validator.prototype.customFormats.slackmtypes = function slackmtypes(input) {
       return ['message', 'user_typing'].includes(String(input));
     };
+
+    Validator.prototype.customFormats.slackmsubtypes = function slackmtypes(input) {
+      return ['message_changed'].includes(String(input));
+    };
+
     /* eslint-enable no-param-reassign */
     return Validator;
+  },
+  CustomJSONSchemaValidatorAddRefs(validatorInstance) {
+    const ChatUpdateMessageEditedSchema = {
+      id: '/ChatUpdateMessageEdited',
+      type: 'object',
+      properties: {
+        user: { type: 'string', format: 'slackuid' },
+        ts: { type: 'string', format: 'slackts' }
+      },
+      required: ['user', 'ts']
+    };
+    validatorInstance.addSchema(ChatUpdateMessageEditedSchema, ChatUpdateMessageEditedSchema.id);
+
+    const ChatUpdateMessageSchema = {
+      id: '/ChatUpdateMessage',
+      type: 'object',
+      properties: {
+        client_msg_id: { type: 'string', format: 'uuid' },
+        type: { type: 'string', format: 'slackmtypes' },
+        user: { type: 'string', format: 'slackuid' },
+        team: { type: 'string', format: 'slacktid' },
+        edited: { $ref: '/ChatUpdateMessageEdited' },
+        user_team: { type: 'string', format: 'slacktid' },
+        source_team: { type: 'string', format: 'slacktid' },
+        channel: { type: 'string', format: 'slackchid' },
+        text: { type: 'string' },
+        ts: { type: 'string', format: 'slackts' }
+      },
+      required: ['client_msg_id', 'type', 'user', 'team', 'edited', 'user_team', 'source_team', 'channel', 'text', 'ts']
+    };
+    validatorInstance.addSchema(ChatUpdateMessageSchema, ChatUpdateMessageSchema.id);
+
+    const ChatUpdatePreviousMessageSchema = {
+      id: '/ChatUpdatePreviousMessage',
+      type: 'object',
+      properties: {
+        client_msg_id: { type: 'string', format: 'uuid' },
+        type: { type: 'string', format: 'slackmtypes' },
+        user: { type: 'string', format: 'slackuid' },
+        team: { type: 'string', format: 'slacktid' },
+        text: { type: 'string' },
+        ts: { type: 'string', format: 'slackts' }
+      },
+      required: ['client_msg_id', 'type', 'user', 'team', 'ts', 'text']
+    };
+    validatorInstance.addSchema(ChatUpdatePreviousMessageSchema, ChatUpdatePreviousMessageSchema.id);
+
+    return validatorInstance;
   }
 };
